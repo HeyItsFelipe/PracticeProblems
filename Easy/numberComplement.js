15072b1234a4e5ab4363ea13ddd7d9e5dee809c9
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-07-13 21:36:10 -0700

    Adds solved numberComplement.

---
+++ Easy/numberComplement.js
@@ -0,0 +1,41 @@
+/*
+
+https://leetcode.com/problems/number-complement/description/
+
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+*/
+
+const findComplement = num => {
+  return num
+    .toString(2)
+    .split('')
+    .reduce((total, element, index, array) => {
+      if (element === '0') {
+        total += Math.pow(2, array.length - 1 - index);
+      }
+      return total;
+    }, 0);
+};
+
+console.log(findComplement(5));
+console.log(findComplement(1));
+
+/*
+The function performs three linear tasks in succession.
+Dropping the coefficient, the time complexity is linear, O(n).
+*/

7abe8a4ab0c12f8c6bae2046af61dc8edbf1a221
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-08-06 22:14:45 -0700

    Adds solved maxConsecutiveOnes.

---
+++ Easy/maxConsecutiveOnes.js
@@ -0,0 +1,43 @@
+/*
+
+https://leetcode.com/problems/max-consecutive-ones/description/
+
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.  The maximum number of consecutive 1s is 3.
+
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000
+
+*/
+
+const findMaxConsecutiveOnes = nums => {
+  let max = 0;
+  let current = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      current++;
+    } else {
+      if (current > max) {
+        max = current;
+      }
+      current = 0;
+    }
+  }
+  if (current > max) {
+    max = current;
+  }
+  return max;
+};
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+
+/*
+We are iterating throught the entire input once.
+This function is linear, O(n).
+*/

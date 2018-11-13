4b4e34f5422f6d324999ed2e11416d75931ccd38
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-11-12 20:29:40 -0800

    Refactored LinkedList.

--- DataStructures/LinkedList.js
+++ DataStructures/LinkedList.js
@@ -1,5 +1,9 @@
 class Node {
     constructor(value) {
+        if (value === undefined) {
+            console.log('Cannot create Node from undefined!');
+            return;
+        }
         this.value = value;
         this.next = null;
     }
@@ -16,7 +20,10 @@ class LinkedList {
     }
 
     insert(value) {
-        if (this.head === null) {
+        if (value === undefined) {
+            console.log('Argument needed for insert!');
+            return this;
+        } else if (this.head === null) {
             this.head = new Node(value);
         } else {
             let node = this.head;
@@ -30,34 +37,47 @@ class LinkedList {
     }
 
     remove(value) {
-        if (this.head.value === value) {
+        if (value === undefined) {
+            console.log('Argument needed for remove!');
+            return this;
+        } else if (this.head === null) {
+            console.log(`Cannot remove! List is empty!`);
+            return this;
+        } else if (this.head.value === value) {
             this.head = this.head.next;
+            this.length--;
+            return this;
         } else {
             let currentNode = this.head;
             let previousNode;
-            while (currentNode.value !== value) {
-                previousNode = currentNode;
-                currentNode = currentNode.next;
-                if (currentNode === null) {
-                    console.log(`Cannot delete! Value ${value} not found!`);
+            while (currentNode !== null) {
+                if (currentNode.value === value) {
+                    previousNode.next = currentNode.next;
+                    this.length--;
                     return this;
                 }
+                previousNode = currentNode;
+                currentNode = currentNode.next;
             }
-            previousNode.next = currentNode.next;
         }
-        this.length--;
+        console.log(`Cannot delete! Value ${value} not found!`);
         return this;
     }
 
     contains(value) {
-        let currentNode = this.head;
-        while (currentNode.value !== value) {
-            currentNode = currentNode.next;
-            if (currentNode === null) {
-                return false;
+        if (value === undefined) {
+            console.log('Argument needed for contains!');
+            return false;
+        } else {
+            let node = this.head;
+            while (node !== null) {
+                if (node.value === value) {
+                    return true;
+                }
+                node = node.next;
             }
+            return false;
         }
-        return true;
     }
 
     getLength() {
@@ -71,15 +91,15 @@ list.insert(5);
 list.insert(8);
 console.log(list);
 console.log(list.getLength());
-list.remove(0);
+list.remove();
 console.log(list);
 let list2 = new LinkedList(4);
 list2.insert(1);
 list2.insert(9);
 console.log(list2);
 console.log(list);
-console.log(list.contains(0));
-console.log(list.contains(1));
+console.log(list.contains());
+console.log(list.contains(5));
 let list3 = new LinkedList();
 console.log(list3);
 list3.insert(7);
@@ -88,3 +108,9 @@ list3.remove(7);
 console.log(list3);
 list3.insert(1).insert(2).insert(3).remove(0).insert(4);
 console.log(list3);
+let list4 = new LinkedList();
+console.log(list4);
+list4.remove(3);
+console.log(list4.contains(3));
+list4.insert(3);
+console.log(list4);

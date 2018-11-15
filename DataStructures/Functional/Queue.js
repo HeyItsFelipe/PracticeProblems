fb6e9e8fb5cdd40f955d867dbf057e147745c4fd
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-11-15 10:50:04 -0800

    Renames Prototypal folder to Functional and adds new data structures.

---
+++ DataStructures/Functional/Queue.js
@@ -0,0 +1,59 @@
+function Queue(value) {
+    this._storage = {};
+    this._size = 0;
+    this._headIndex = 0;
+    if (value !== undefined) {
+        this.enqueue(value);
+    }
+}
+
+Queue.prototype.enqueue = function (value) {
+    if (value === undefined) {
+        console.log('Cannot enqueue undefined value!');
+        return;
+    }
+    this._storage[this._size + this._headIndex] = value;
+    this._size++;
+}
+
+Queue.prototype.dequeue = function () {
+    if (this._size === 0) {
+        console.log('Cannot dequeue from empty Queue!');
+        return;
+    }
+    let deq = this._storage[this._headIndex];
+    delete this._storage[this._headIndex];
+    this._headIndex++;
+    this._size--;
+    return deq;
+}
+
+Queue.prototype.peek = function () {
+    if (this._size === 0) {
+        console.log('Peeked at empty Queue!');
+        return;
+    }
+    return this._storage[this._headIndex];
+}
+
+Queue.prototype.getSize = function () {
+    return this._size;
+}
+
+let q = new Queue();
+q.enqueue(4);
+q.enqueue(7);
+q.enqueue(9);
+console.log(q);
+q.dequeue();
+console.log(q.peek());
+console.log(q.getSize())
+let q2 = new Queue(2);
+q2.enqueue(4);
+console.log(q2);
+q2.dequeue();
+q2.dequeue();
+q2.dequeue();
+console.log(q2);
+q2.enqueue(6);
+console.log(q2);
---
+++ DataStructures/Functional/Stack.js
@@ -0,0 +1,55 @@
+function Stack(value) {
+    this._storage = {};
+    this._size = 0;
+    if (value !== undefined) {
+        this.push(value);
+    }
+}
+
+Stack.prototype.push = function (value) {
+    if (value === undefined) {
+        console.log('Cannot push undefined value!');
+        return;
+    }
+    this._storage[this._size] = value;
+    this._size++;
+}
+
+Stack.prototype.pop = function () {
+    if (this._size === 0) {
+        console.log('Cannot pop from empty Stack!');
+        return;
+    }
+    let popped = this._storage[this._size - 1];
+    delete this._storage[this._size - 1];
+    this._size--;
+    return popped;
+}
+
+Stack.prototype.peek = function () {
+    if (this._size === 0) {
+        console.log('Peeked at empty Stack!');
+    }
+    return this._storage[this._size - 1];
+}
+
+Stack.prototype.getSize = function () {
+    return this._size;
+}
+
+let s = new Stack();
+s.push(3);
+s.push(7);
+console.log(s);
+console.log(s.peek());
+s.pop();
+console.log(s);
+let s2 = new Stack(4);
+s2.push(7);
+console.log(s2);
+s2.pop();
+s2.pop();
+s2.pop();
+s2.push();
+console.log(s2.peek());
+console.log(s2.getSize());

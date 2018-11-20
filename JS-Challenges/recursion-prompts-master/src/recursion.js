a2d3111831393594ebeeed7b6c2b9c2b0dfbc3ac
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-11-19 20:07:58 -0800

    Gets up to number 18 done in JS recursion.

--- JS-Challenges/recursion-prompts-master/src/recursion.js
+++ JS-Challenges/recursion-prompts-master/src/recursion.js
@@ -218,21 +218,40 @@ var gcd = function (x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function (str1, str2) {
+    if (str1.length === 0 && str2.length === 0) {
+        return true;
+    }
+    if (str1[0] !== str2[0]) {
+        return false;
+    }
+    return compareStr(str1.slice(1), str2.slice(1))
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function (str) {
+    if (str.length === 0) {
+        return [];
+    }
+    return [str[0]].concat(createArray(str.slice(1)));
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function (array) {
+    if (array.length === 0) {
+        return [];
+    }
+    return [array[array.length - 1]].concat(reverseArr(array.slice(0, -1)));
 };
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function (value, length) {
+    if (length === 0) {
+        return [];
+    }
+    return [value].concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
